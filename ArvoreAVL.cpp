e97c1aad90a6792982df4475a68a98c2d01562bf
YaghoMattos <mattosyagho@gmail.com>
2021-07-11 21:58:33 -0300

    altura da árvore

--- ArvoreAVL.cpp
+++ ArvoreAVL.cpp
@@ -2,53 +2,61 @@
 
 using namespace std;
 
-ArvoreAVL::ArvoreAVL(){
+ArvoreAVL::ArvoreAVL()
+{
     this->raiz = NULL;
-}    
+}
 
-ArvoreAVL::~ArvoreAVL(){
+ArvoreAVL::~ArvoreAVL()
+{
     this->limpa();
-} 
+}
 
-void ArvoreAVL::limpa(){
+void ArvoreAVL::limpa()
+{
     delete raiz;
     this->raiz = NULL;
-} 
+}
 
-bool ArvoreAVL::busca(int chave){
-    NoAVL* atual = this->raiz;
+bool ArvoreAVL::busca(int chave)
+{
+    NoAVL *atual = this->raiz;
 
-    while(atual != NULL){
+    while (atual != NULL)
+    {
         // compara
-        if(chave > atual->chave)
+        if (chave > atual->chave)
             atual = atual->sub[NoAVL::DIR];
-        else if(chave < atual->chave)
+        else if (chave < atual->chave)
             atual = atual->sub[NoAVL::ESQ];
         else
             return true;
     }
     return false;
-} 
+}
 
 // Retorna a direção oposta à direção dada
 
-inline NoAVL::Direction ArvoreAVL::oposto(NoAVL::Direction dir){
+inline NoAVL::Direction ArvoreAVL::oposto(NoAVL::Direction dir)
+{
     return (dir == NoAVL::DIR) ? NoAVL::ESQ : NoAVL::DIR;
 }
 
 // Realiza uma rotação simples numa determinada direção.  aceita como parâmetro um nó onde a operação será efetuada e uma direção (esquerda ou direita) para realizar a rotação.
-void ArvoreAVL::rotacionaUmaVez(NoAVL*& no, NoAVL::Direction dir){
+void ArvoreAVL::rotacionaUmaVez(NoAVL *&no, NoAVL::Direction dir)
+{
     int oposto = this->oposto(dir);
-    NoAVL* filho = no->sub[dir];
+    NoAVL *filho = no->sub[dir];
     no->sub[dir] = filho->sub[oposto];
     filho->sub[oposto] = no;
     no = filho;
 }
 
 // Realiza uma rotação dupla numa determinada direção.  aceita como parâmetro um nó onde a operação será efetuada  e uma direção (esquerda ou direita) para realizar a rotação.
-void ArvoreAVL::rotacionaDuasVezes(NoAVL*& no, NoAVL::Direction dir){
+void ArvoreAVL::rotacionaDuasVezes(NoAVL *&no, NoAVL::Direction dir)
+{
     int oposto = this->oposto(dir);
-    NoAVL* filho = no->sub[dir]->sub[oposto];
+    NoAVL *filho = no->sub[dir]->sub[oposto];
     no->sub[dir]->sub[oposto] = filho->sub[dir];
     filho->sub[dir] = no->sub[dir];
     no->sub[dir] = filho;
@@ -60,33 +68,37 @@ void ArvoreAVL::rotacionaDuasVezes(NoAVL*& no, NoAVL::Direction dir){
 
 // Atualiza os fatores de balanceamento após uma rotação
 
-void ArvoreAVL::atualizaBalancear(NoAVL* no, NoAVL::Direction dir){
+void ArvoreAVL::atualizaBalancear(NoAVL *no, NoAVL::Direction dir)
+{
     int oposto = this->oposto(dir);
     int bal = no->sub[dir]->sub[oposto]->balanco;
     // Se o fator está pesado no mesmo lado
-    if (bal == dir){
+    if (bal == dir)
+    {
         no->balanco = NoAVL::IGUAL;
         no->sub[dir]->balanco = oposto;
     }
     // Se o fator está pesado do lado oposto
-    else if (bal == oposto){
+    else if (bal == oposto)
+    {
         no->balanco = dir;
         no->sub[dir]->balanco = NoAVL::IGUAL;
-    }else // O fator de balanceamento está igual
+    }
+    else // O fator de balanceamento está igual
     {
         no->balanco = no->sub[dir]->balanco = NoAVL::IGUAL;
     }
     no->sub[dir]->sub[oposto]->balanco = NoAVL::IGUAL;
-
-} 
+}
 
 // Efetua o rebalanceamento após uma operação de inserção
-void ArvoreAVL::rebalancearInsere(NoAVL*& no, NoAVL::Direction dir, bool& balancear){
+void ArvoreAVL::rebalancearInsere(NoAVL *&no, NoAVL::Direction dir, bool &balancear)
+{
     int oposto = this->oposto(dir);
     // Se o fator de balanceamento do nó era
     //  igual a direção em que houve a inserção,
     if (no->balanco == dir) // (ou seja o nó foi inserido
-    {   //  na subárvore de maior altura
+    {                       //  na subárvore de maior altura
         // Temos 2 casos:
         if (no->sub[dir]->balanco == dir)
         {
@@ -109,22 +121,24 @@ void ArvoreAVL::rebalancearInsere(NoAVL*& no, NoAVL::Direction dir, bool& balanc
         balancear = false;
     }
     else // Se não, o nó já estava equilibrado
-    { // e agora seu equilibrio foi deslocado.
+    {    // e agora seu equilibrio foi deslocado.
         no->balanco = dir;
     }
 }
 
-// Insere um novo item na árvore 
-void ArvoreAVL::insere(int chave){
+// Insere um novo item na árvore
+void ArvoreAVL::insere(int chave)
+{
     bool balancear = false; // Para passagem por referência
     // Chamamos o método recursivo para realizar a operação
     this->insere(chave, this->raiz, balancear);
 }
 
 // Insere um novo item na árvore (recursivo)
-void ArvoreAVL::insere(int chave, NoAVL*& no, bool& balancear){   // Se o nó atual é nulo,
+void ArvoreAVL::insere(int chave, NoAVL *&no, bool &balancear)
+{ // Se o nó atual é nulo,
     if (no == NULL)
-    {   // é aqui mesmo onde vamos inserir
+    { // é aqui mesmo onde vamos inserir
         no = new NoAVL(chave);
         // marcamos que a altura mudou, é
         //  preciso chegar o balanceamento
@@ -136,31 +150,51 @@ void ArvoreAVL::insere(int chave, NoAVL*& no, bool& balancear){   // Se o nó at
         return;
     }
     else // Ainda não chegamos onde queríamos
-    {// Prosseguimos com a pesquisa binária
+    {    // Prosseguimos com a pesquisa binária
         NoAVL::Direction dir = (chave > no->chave) ? NoAVL::DIR : NoAVL::ESQ;
         balancear = false; // preparamos e chamamos recursão
         insere(chave, no->sub[dir], balancear);
         if (balancear) // Se a altura mudou,
-        {// Efetuamos o rebalanceamento
+        {              // Efetuamos o rebalanceamento
             this->rebalancearInsere(no, dir, balancear);
         }
     }
 }
 
-void ArvoreAVL::printAVL(const string& prefix, const NoAVL* no, bool isLeft){
-    if(no != nullptr ){
+void ArvoreAVL::printAVL(const string &prefix, const NoAVL *no, bool isLeft)
+{
+    if (no != nullptr)
+    {
         cout << prefix;
-        cout << (isLeft ? "├──" : "└──" );
+        cout << (isLeft ? "├──" : "└──");
         // print the value of the node
         cout << no->chave << endl;
         // enter the next tree level - left and right branch
-        printAVL( prefix + (isLeft ? "│   " : "    "), no->sub[NoAVL::ESQ], true);
-        printAVL( prefix + (isLeft ? "│   " : "    "), no->sub[NoAVL::DIR], false);
+        printAVL(prefix + (isLeft ? "│   " : "    "), no->sub[NoAVL::ESQ], true);
+        printAVL(prefix + (isLeft ? "│   " : "    "), no->sub[NoAVL::DIR], false);
     }
 }
 
-void ArvoreAVL::imprime(){
-    printAVL("", this->raiz, false);    
+void ArvoreAVL::imprime()
+{
+    printAVL("", this->raiz, false);
+    cout << "altura da arvore -> " << calcAltura(this->raiz) << endl;
 }
 
+int ArvoreAVL::calcAltura(NoAVL *raiz)
+{
+    if (raiz != NULL)
+    {
+        //recursao
+        int altEsq = calcAltura(raiz->sub[0]);
+        int altDir = calcAltura(raiz->sub[1]);
 
+        //verifica quem é maior
+        if (altEsq > altDir)
+            return (altEsq + 1);
+        else
+            return (altDir + 1);
+    }
+    else
+        return 0;
+}
--- ArvoreAVL.h
+++ ArvoreAVL.h
@@ -6,29 +6,32 @@
 
 using namespace std;
 
-class ArvoreAVL{
+class ArvoreAVL
+{
 
-    public:
-        ArvoreAVL();
-        ~ArvoreAVL();
-        void insere(int chave);
-        // procura chave 
-        bool busca(int chave);
-        void limpa();
-        void imprime();
-    private:
-        // Raiz da arvore
-        NoAVL* raiz;
-        // indica se deve balancear ou nao
-        bool balancear;
-        inline NoAVL::Direction oposto(NoAVL::Direction dir);
-        // Inserção e remoção recursivos
-        void insere(int chave, NoAVL*& no, bool& balancear);
-        bool remove(int chave, NoAVL*& no, bool& balancear);
-        // balanceamento
-        void rotacionaUmaVez(NoAVL*& no, NoAVL::Direction dir);
-        void rotacionaDuasVezes(NoAVL*& no, NoAVL::Direction dir);
-        void atualizaBalancear(NoAVL* no, NoAVL::Direction dir);
-        void rebalancearInsere(NoAVL*& node, NoAVL::Direction dir, bool& balancear);
-        void printAVL(const string& prefix, const NoAVL* no, bool isLeft);
+public:
+    ArvoreAVL();
+    ~ArvoreAVL();
+    void insere(int chave);
+    // procura chave
+    bool busca(int chave);
+    void limpa();
+    void imprime();
+
+private:
+    // Raiz da arvore
+    NoAVL *raiz;
+    // indica se deve balancear ou nao
+    bool balancear;
+    inline NoAVL::Direction oposto(NoAVL::Direction dir);
+    // Inserção e remoção recursivos
+    void insere(int chave, NoAVL *&no, bool &balancear);
+    bool remove(int chave, NoAVL *&no, bool &balancear);
+    // balanceamento
+    void rotacionaUmaVez(NoAVL *&no, NoAVL::Direction dir);
+    void rotacionaDuasVezes(NoAVL *&no, NoAVL::Direction dir);
+    void atualizaBalancear(NoAVL *no, NoAVL::Direction dir);
+    void rebalancearInsere(NoAVL *&node, NoAVL::Direction dir, bool &balancear);
+    void printAVL(const string &prefix, const NoAVL *no, bool isLeft);
+    int calcAltura(NoAVL *raiz);
 };
